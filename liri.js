f28c03ac057639d537c77c8530e73c69b4d27042
sharitacumm <emandraine@yahoo.com>
2019-02-12 11:25:35 -0500

    Added Spotify and Bands

--- liri.js
+++ liri.js
@@ -6,6 +6,31 @@ function movie(){
 
     console.log(response.data);
   })
+  }
+
+function bands(){
+    axios.get("https://rest.bandsintown.com/artists/smino/events?app_id=codingbootcamp&date=upcoming").then(function (response) {
+
+    console.log(response.data);
+  })
+}
+
+function spotify(){
+    var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id:  "c46fc08e125c4cddafe68989c9d152a7",
+  secret: "f6dbd304e9954d01bfc06b608b6ab441",  
+});
+ 
+spotify.search({ type: 'track', query: process.argv[3]}, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data.tracks.items[0].album.artists); 
+});
+
 }
 
 if (process.argv[2] === "movie") {
@@ -14,7 +39,9 @@ if (process.argv[2] === "movie") {
 }
 else if (process.argv[2] === "songs") {
     console.log("do spotify stuff")
+    spotify()
 }
 else if (process.argv[2] === "concerts") {
     console.log("do band stuff")
+    bands()
 }
